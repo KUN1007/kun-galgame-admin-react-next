5d812914422cdd0bb778acf58fe893a0f1632df3
KUN1007 <yuyuyukunkunkun@gmail.com>
2024-02-06 17:36:41 +0800

    mod: remove radix ui

--- src/components/providers/NextThemesProvider.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { ThemeProvider } from 'next-themes'
-import { ThemeProviderProps } from 'next-themes/dist/types'
-
-export function NextThemesProvider({ children, ...props }: ThemeProviderProps) {
-  return <ThemeProvider {...props}>{children}</ThemeProvider>
-}
